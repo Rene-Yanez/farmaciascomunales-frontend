2190b348c46490bf367077f0fcb467701524200f
Kato Ignacio Bello Martínez <katherinne.bello2101@alumnos.ubiobio.cl>
2025-06-23 03:50:39 -0400

    gggg

--- src/pages/CrearUsuario.jsx
+++ src/pages/CrearUsuario.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { apiFetch } from '../service/api'
+import { apiFetch } from '../services/api'
 
 function CrearUsuario() {
   const [nombre, setNombre] = useState('')
